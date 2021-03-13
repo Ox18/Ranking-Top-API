1c5fb57d0f84d2c24334f6a27227cbb5b14be0e6
Alex <73305665+alexwathever@users.noreply.github.com>
2021-03-13 01:47:43 -0500

    cors add

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +10,7 @@ app.set('json spaces', 2);
 // Router
 app.use('/api/tranking', require('./routes/tRanking'));
 // Middlewares
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
